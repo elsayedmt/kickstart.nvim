0f9be814bdc4116fb0e0d27907e362b8b773c18d
Mohamed <elsayedmt@gmail.com>
2024-08-29 12:24:33 -0400

    Added crates + rust-tools mrcjkb fork

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,17 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  --  {
+  --    'saecki/crates.nvim',
+  --    tag = 'stable',
+  --    config = function()
+  --      require('crates').setup()
+  --    end,
+  --  },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+}
