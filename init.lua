0d158998da62473e9b6f6b4e53f5a97ffd836eaa
Mohamed <elsayedmt@gmail.com>
2024-08-30 14:15:23 -0400

    Added tabs + some imports

--- init.lua
+++ init.lua
@@ -604,6 +604,8 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        terraformls = {},
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
