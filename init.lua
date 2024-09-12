5b2ef9b6375a6db81cd245ee153c1c1c07d9bb2a
Mohamed Elsayed <melsayed@exchangesolutions.com>
2024-09-12 09:25:04 -0400

    Added typescript and yaml lsp

--- init.lua
+++ init.lua
@@ -609,6 +609,7 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        yamlls = {},
         terraformls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
