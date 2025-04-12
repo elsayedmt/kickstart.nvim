0062030405fc7682c04ef52b00ee217a7bb549d7
Mohamed <elsayedmt@gmail.com>
2025-04-12 14:13:47 -0400

    Removed yamlls

--- init.lua
+++ init.lua
@@ -604,7 +604,6 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
-        yamlls = {},
         terraformls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
