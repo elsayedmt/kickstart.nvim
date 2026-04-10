ac2fe3be54c2639052f6904d6c20d3b17f597e4a
Mohamed Elsayed <melsayed@exchangesolutions.com>
2026-04-10 10:20:13 -0400

    tmux configs for opencode

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -9,7 +9,12 @@ return {
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
-      -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      provider = {
+        enabled = 'tmux',
+        tmux = {
+          options = '-h', -- Open in a horizontal split (or use '-v' for vertical)
+        },
+      },
     }
 
     -- Required for `opts.events.reload`.
