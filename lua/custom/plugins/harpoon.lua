df8f825e130af7e7fb17f4c5caac6be48cc535ca
Mohamed Elsayed <melsayed@exchangesolutions.com>
2026-06-13 13:38:15 -0400

    feat(harpoon): convert to vim.pack (harpoon2 branch)

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,85 +1,23 @@
--- Harpoon 2: Quick file navigation and marking
--- Mark your most frequently used files and jump between them instantly
-
-return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  keys = {
-    {
-      '<leader>ma',
-      function()
-        require('harpoon'):list():add()
-      end,
-      desc = '[M]ark: [A]dd file to Harpoon',
-    },
-    {
-      '<leader>mm',
-      function()
-        local harpoon = require 'harpoon'
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = '[M]ark: Toggle Harpoon [M]enu',
-    },
-    {
-      '<leader>1',
-      function()
-        require('harpoon'):list():select(1)
-      end,
-      desc = '[M]ark: Jump to file 1',
-    },
-    {
-      '<leader>2',
-      function()
-        require('harpoon'):list():select(2)
-      end,
-      desc = '[M]ark: Jump to file 2',
-    },
-    {
-      '<leader>3',
-      function()
-        require('harpoon'):list():select(3)
-      end,
-      desc = '[M]ark: Jump to file 3',
-    },
-    {
-      '<leader>4',
-      function()
-        require('harpoon'):list():select(4)
-      end,
-      desc = '[M]ark: Jump to file 4',
-    },
-    -- Navigate between marked files
-    {
-      '<leader>mp',
-      function()
-        require('harpoon'):list():prev()
-      end,
-      desc = '[M]ark: [P]revious file',
-    },
-    {
-      '<leader>mn',
-      function()
-        require('harpoon'):list():next()
-      end,
-      desc = '[M]ark: [N]ext file',
-    },
-    -- Clear all marks
-    {
-      '<leader>mc',
-      function()
-        require('harpoon'):list():clear()
-      end,
-      desc = '[M]ark: [C]lear all marks',
-    },
-  },
-  config = function()
-    local harpoon = require 'harpoon'
-    harpoon:setup {
-      settings = {
-        save_on_toggle = true,
-        sync_on_ui_close = true,
-      },
-    }
-  end,
+-- Harpoon 2: quick file marking & navigation
+vim.pack.add {
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/ThePrimeagen/harpoon', version = 'harpoon2' },
 }
+
+local harpoon = require 'harpoon'
+harpoon:setup {
+  settings = {
+    save_on_toggle = true,
+    sync_on_ui_close = true,
+  },
+}
+
+vim.keymap.set('n', '<leader>ma', function() harpoon:list():add() end, { desc = '[M]ark: [A]dd file to Harpoon' })
+vim.keymap.set('n', '<leader>mm', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = '[M]ark: Toggle Harpoon [M]enu' })
+vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end, { desc = '[M]ark: Jump to file 1' })
+vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end, { desc = '[M]ark: Jump to file 2' })
+vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end, { desc = '[M]ark: Jump to file 3' })
+vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end, { desc = '[M]ark: Jump to file 4' })
+vim.keymap.set('n', '<leader>mp', function() harpoon:list():prev() end, { desc = '[M]ark: [P]revious file' })
+vim.keymap.set('n', '<leader>mn', function() harpoon:list():next() end, { desc = '[M]ark: [N]ext file' })
+vim.keymap.set('n', '<leader>mc', function() harpoon:list():clear() end, { desc = '[M]ark: [C]lear all marks' })
