8641f428d81fdb8ebb0feab1d2099a977ffd28b8
Mohamed Elsayed <melsayed@exchangesolutions.com>
2026-06-13 13:38:11 -0400

    feat(barbar): convert to vim.pack

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -1,14 +1,12 @@
-local key_maps = function()
+-- barbar.nvim: bufferline with Alt-key buffer navigation
+local function key_maps()
   local map = vim.api.nvim_set_keymap
   local opts = { noremap = true, silent = true }
 
-  -- Move to previous/next
   map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
   map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
-  -- Re-order to previous/next
   map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
   map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
-  -- Goto buffer in position...
   map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
   map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
   map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
@@ -19,21 +17,9 @@ local key_maps = function()
   map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
   map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
   map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
-  -- Pin/unpin buffer
   map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
-  -- Close buffer
   map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
-  -- Wipeout buffer
-  --                 :BufferWipeout
-  -- Close commands
-  --                 :BufferCloseAllButCurrent
-  --                 :BufferCloseAllButPinned
-  --                 :BufferCloseAllButCurrentOrPinned
-  --                 :BufferCloseBuffersLeft
-  --                 :BufferCloseBuffersRight
-  -- Magic buffer-picking mode
   map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
-  -- Sort automatically by... (changed <Space>bd to <Space>bD to avoid conflict)
   map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
   map('n', '<Space>bn', '<Cmd>BufferOrderByName<CR>', opts)
   map('n', '<Space>bD', '<Cmd>BufferOrderByDirectory<CR>', opts)
@@ -41,24 +27,8 @@ local key_maps = function()
   map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 end
 
-return {
+vim.g.barbar_auto_setup = false
+key_maps()
 
-  {
-    'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function()
-      vim.g.barbar_auto_setup = false
-      key_maps()
-    end,
-    opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  },
-}
+vim.pack.add { { src = 'https://github.com/romgrk/barbar.nvim' } }
+require('barbar').setup {}
