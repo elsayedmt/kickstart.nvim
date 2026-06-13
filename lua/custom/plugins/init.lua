7111de7ecae9d217747b687ede1dcb6f8d9647e9
Mohamed Elsayed <melsayed@exchangesolutions.com>
2026-06-13 13:38:35 -0400

    refactor(custom): terraform autocmds + restore directory loader loop

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,29 +1,18 @@
+-- Terraform / HCL filetype detection
 vim.cmd [[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]]
 vim.cmd [[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]]
 vim.cmd [[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]]
 vim.cmd [[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]]
 vim.cmd [[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]]
 
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
+-- Per-plugin files in this directory are auto-required by the loader.
+-- crates.nvim and rustaceanvim are loaded lazily from after/ftplugin/{toml,rust}.lua.
 
--- See the kickstart.nvim README for more information
-
----@module 'lazy'
----@type LazySpec
-return {
-  {
-    'saecki/crates.nvim',
-    tag = 'stable',
-    config = function()
-      require('crates').setup()
-    end,
-  },
-  {
-    'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
-    lazy = false, -- This plugin is already lazy
-    ft = 'rust',
-    dependencies = 'neovim/nvim-lspconfig',
-  },
-}
+-- Iterate over all Lua files in the plugins directory and load them
+local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
+for file_name, type in vim.fs.dir(plugins_dir, { follow = true }) do
+  if (type == 'file' or type == 'link') and file_name:match '%.lua$' and file_name ~= 'init.lua' then
+    local module = file_name:gsub('%.lua$', '')
+    require('custom.plugins.' .. module)
+  end
+end
