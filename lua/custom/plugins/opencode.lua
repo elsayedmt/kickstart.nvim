d61aa96ee08879c0525f53deb3e927ee28d0d30b
Mohamed Elsayed <melsayed@exchangesolutions.com>
2026-06-13 13:38:27 -0400

    feat(opencode): convert to vim.pack

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -1,52 +1,22 @@
-return {
-  'NickvanDyke/opencode.nvim',
-  dependencies = {
-    -- Recommended for `ask()` and `select()`.
-    -- Required for `snacks` provider.
-    ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
-    { 'folke/snacks.nvim', opts = { input = {}, picker = {}, terminal = {} } },
-  },
-  config = function()
-    ---@type opencode.Opts
-    vim.g.opencode_opts = {
-      provider = {
-        enabled = 'tmux',
-        tmux = {
-          options = '-h', -- Open in a horizontal split (or use '-v' for vertical)
-        },
-      },
-    }
-
-    -- Required for `opts.events.reload`.
-    vim.o.autoread = true
-
-    -- OpenCode keymaps (using <leader>o prefix to avoid conflicts with Vim defaults)
-    vim.keymap.set({ 'n', 'x' }, '<leader>oa', function()
-      require('opencode').ask('@this: ', { submit = true })
-    end, { desc = '[O]pencode [A]sk' })
-    vim.keymap.set({ 'n', 'x' }, '<leader>ox', function()
-      require('opencode').select()
-    end, { desc = '[O]pencode E[x]ecute action' })
-    vim.keymap.set({ 'n', 'x' }, '<leader>op', function()
-      require('opencode').prompt '@this'
-    end, { desc = '[O]pencode [P]rompt (add to chat)' })
-    vim.keymap.set({ 'n', 't' }, '<leader>oo', function()
-      require('opencode').toggle()
-    end, { desc = '[O]pencode Toggle' })
-    -- Also keep <C-.> as it's convenient and doesn't conflict
-    vim.keymap.set({ 'n', 't' }, '<C-.>', function()
-      require('opencode').toggle()
-    end, { desc = 'Toggle opencode' })
-    
-    -- OpenCode navigation (optional - you can use regular scrolling in the opencode window)
-    vim.keymap.set('n', '<leader>ou', function()
-      require('opencode').command 'session.half.page.up'
-    end, { desc = '[O]pencode half page [U]p' })
-    vim.keymap.set('n', '<leader>od', function()
-      require('opencode').command 'session.half.page.down'
-    end, { desc = '[O]pencode half page [D]own' })
-    
-    -- No need to remap + and - since we're not overriding <C-a> and <C-x> anymore
-    -- Standard Vim increment/decrement (<C-a>/<C-x>) remain untouched
-  end,
+-- opencode.nvim: opencode integration (tmux provider)
+vim.pack.add {
+  { src = 'https://github.com/folke/snacks.nvim' },
+  { src = 'https://github.com/NickvanDyke/opencode.nvim' },
 }
+
+vim.g.opencode_opts = {
+  provider = {
+    enabled = 'tmux',
+    tmux = { options = '-h' },
+  },
+}
+
+vim.o.autoread = true
+
+vim.keymap.set({ 'n', 'x' }, '<leader>oa', function() require('opencode').ask('@this: ', { submit = true }) end, { desc = '[O]pencode [A]sk' })
+vim.keymap.set({ 'n', 'x' }, '<leader>ox', function() require('opencode').select() end, { desc = '[O]pencode E[x]ecute action' })
+vim.keymap.set({ 'n', 'x' }, '<leader>op', function() require('opencode').prompt '@this' end, { desc = '[O]pencode [P]rompt (add to chat)' })
+vim.keymap.set({ 'n', 't' }, '<leader>oo', function() require('opencode').toggle() end, { desc = '[O]pencode Toggle' })
+vim.keymap.set({ 'n', 't' }, '<C-.>', function() require('opencode').toggle() end, { desc = 'Toggle opencode' })
+vim.keymap.set('n', '<leader>ou', function() require('opencode').command 'session.half.page.up' end, { desc = '[O]pencode half page [U]p' })
+vim.keymap.set('n', '<leader>od', function() require('opencode').command 'session.half.page.down' end, { desc = '[O]pencode half page [D]own' })
