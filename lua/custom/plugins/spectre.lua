232149e0a42b8c3c6ba9a7bbb112017bd1f7341e
Mohamed Elsayed <melsayed@exchangesolutions.com>
2026-06-13 13:40:49 -0400

    feat(spectre): convert to vim.pack (deferred on first use)

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -1,65 +1,44 @@
--- nvim-spectre: Search and replace across project
--- WARNING: Always commit your files before replacing text (no undo support)
+-- nvim-spectre: project search & replace (deferred to first use)
+local loaded = false
+local function ensure()
+  if not loaded then
+    vim.pack.add {
+      { src = 'https://github.com/nvim-lua/plenary.nvim' },
+      { src = 'https://github.com/nvim-pack/nvim-spectre' },
+    }
+    require('spectre').setup {
+      color_devicons = true,
+      open_cmd = 'vnew',
+      live_update = false,
+      line_sep_start = '┌-----------------------------------------',
+      result_padding = '¦  ',
+      line_sep = '└-----------------------------------------',
+      highlight = { ui = 'String', search = 'DiffChange', replace = 'DiffDelete' },
+      default = {
+        find = { cmd = 'rg', options = { 'ignore-case' } },
+        replace = { cmd = 'sed' },
+      },
+    }
+    loaded = true
+  end
+end
 
-return {
-  'nvim-pack/nvim-spectre',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  cmd = 'Spectre', -- Lazy load on command
-  keys = {
-    {
-      '<leader>SS',
-      function()
-        require('spectre').toggle()
-      end,
-      desc = 'Toggle [S]pectre (search/replace)',
-    },
-    {
-      '<leader>Sw',
-      function()
-        require('spectre').open_visual { select_word = true }
-      end,
-      desc = '[S]pectre: Replace current [W]ord',
-    },
-    {
-      '<leader>Sr',
-      function()
-        require('spectre').open_visual()
-      end,
-      mode = 'v',
-      desc = '[S]pectre: [R]eplace selection',
-    },
-    {
-      '<leader>Sf',
-      function()
-        require('spectre').open_file_search { select_word = true }
-      end,
-      desc = '[S]pectre: Replace in current [F]ile',
-    },
-  },
-  opts = {
-    color_devicons = true,
-    open_cmd = 'vnew',
-    live_update = false, -- auto execute search when you write files
-    line_sep_start = '┌-----------------------------------------',
-    result_padding = '¦  ',
-    line_sep = '└-----------------------------------------',
-    highlight = {
-      ui = 'String',
-      search = 'DiffChange',
-      replace = 'DiffDelete',
-    },
-    -- Use ripgrep for search (make sure it's installed: brew install ripgrep)
-    -- Use sed for replace (macOS users: brew install gnu-sed)
-    default = {
-      find = {
-        cmd = 'rg',
-        options = { 'ignore-case' },
-      },
-      replace = {
-        cmd = 'sed',
-      },
-    },
-  },
-}
+vim.keymap.set('n', '<leader>SS', function()
+  ensure()
+  require('spectre').toggle()
+end, { desc = 'Toggle [S]pectre (search/replace)' })
+
+vim.keymap.set('n', '<leader>Sw', function()
+  ensure()
+  require('spectre').open_visual { select_word = true }
+end, { desc = '[S]pectre: Replace current [W]ord' })
+
+vim.keymap.set('v', '<leader>Sr', function()
+  ensure()
+  require('spectre').open_visual()
+end, { desc = '[S]pectre: [R]eplace selection' })
+
+vim.keymap.set('n', '<leader>Sf', function()
+  ensure()
+  require('spectre').open_file_search { select_word = true }
+end, { desc = '[S]pectre: Replace in current [F]ile' })
